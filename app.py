d6fcefc0c65c418822523767eaa7787c90736a86
kuro474474-jpg <kuro474474@gmail.com>
2026-03-25 00:24:44 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, abort
+from linebot.v3.webhook import WebhookHandler
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.webhooks import MessageEvent, TextMessageContent
+import os
+
+CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
+CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
+
+app = Flask(__name__)
+
+configuration = Configuration(access_token=CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
+
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    signature = request.headers["X-Line-Signature"]
+    body = request.get_data(as_text=True)
+
+    handler.handle(body, signature)
+    return "OK"
+
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    text = event.message.text
+
+    if "尻尻" in text:
+        reply = "好可憐 腦子長水泡"
+    elif "噴噴" in text:
+        reply = "好可憐 腦子長水泡"
+    elif "麻麻" in text:
+        reply = "好可憐 腦子長水泡"
+    elif "尻噴麻" in text:
+        reply = "好可憐 腦子長水泡"
+    else:
+        return
+
+    with ApiClient(configuration) as api_client:
+        MessagingApi(api_client).reply_message(
+            ReplyMessageRequest(
+                replyToken=event.reply_token,
+                messages=[TextMessage(text=reply)]
+            )
+        )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
